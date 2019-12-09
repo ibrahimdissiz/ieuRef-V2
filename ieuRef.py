63535de24b006c7e2c39ee01490ec6a129af243f
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2019-12-09 10:34:03 +0300

    Create dialog created

--- ieuRef.py
+++ ieuRef.py
@@ -4,32 +4,27 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
-class QStringList(object):
-    pass
-
-
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setWindowIcon(QIcon('icon/logo.png'))  #window icon
-
+        self.setWindowIcon(QIcon('icon/logo.png'))
         file_menu = self.menuBar().addMenu("&File")
 
         help_menu = self.menuBar().addMenu("&About")
         self.setWindowTitle("IeuRef")
-        self.setMinimumSize(800, 600)
+        self.setMinimumSize(900, 700)
 
         self.tableWidget = QTableWidget()
         self.setCentralWidget(self.tableWidget)
         self.tableWidget.setAlternatingRowColors(True)
-        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setColumnCount(5)
         self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
         self.tableWidget.horizontalHeader().setSortIndicatorShown(False)
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
         self.tableWidget.verticalHeader().setVisible(False)
         self.tableWidget.verticalHeader().setCascadingSectionResizes(False)
         self.tableWidget.verticalHeader().setStretchLastSection(False)
-        self.tableWidget.setHorizontalHeaderLabels(("Author", "Year", "Type", "Title"))
+        self.tableWidget.setHorizontalHeaderLabels(("No.", "Author", "Year", "Type", "Title"))
 
         toolbar = QToolBar()
         toolbar.setMovable(False)
@@ -38,19 +33,24 @@ class MainWindow(QMainWindow):
         statusbar = QStatusBar()
         self.setStatusBar(statusbar)
 
-        btn_ac_file = QAction(QIcon("icon/file.png"), "Select File", self)  # add student icon
-        btn_ac_file.triggered.connect(self.selectBibtex)
-        btn_ac_file.setStatusTip("Select File")
-        toolbar.addAction(btn_ac_file)
-
-        # btn_ac_refresh = QAction(QIcon("icon/logo.png"), "Refresh", self)  # refresh icon
-        # btn_ac_refresh.triggered.connect(self.loaddata)
-        # btn_ac_refresh.setStatusTip("Refresh Table")
-        # toolbar.addAction(btn_ac_refresh)
-
         selectBibtex_action = QAction(QIcon("icon/addBibtex.png"), "Select BibTeX file", self)
         selectBibtex_action.triggered.connect(self.selectBibtex)
         file_menu.addAction(selectBibtex_action)
+
+        createbibtex_action = QAction(QIcon("icon/create.png"),"Create Bibtex", self)
+        createbibtex_action.triggered.connect(self.createBibtex)
+        file_menu.addAction(createbibtex_action)
+
+    def loadData(self, a, b, c, d):
+        self.tableWidget.setItem(1, 0, QTableWidgetItem(a))
+        self.tableWidget.setItem(1, 1, QTableWidgetItem(b))
+        self.tableWidget.setItem(1, 2, QTableWidgetItem(c))
+        self.tableWidget.setItem(1, 3, QTableWidgetItem(d))
+
+
+    def createBibtex(self):
+        dlg = CreateDialog()
+        dlg.exec()
 
     def selectBibtex(self):
         options = QFileDialog.Options()
@@ -58,9 +58,11 @@ class MainWindow(QMainWindow):
                                                   "BibTeX Files (*.bib)", options=options)
         if bibtexFile:
             print(bibtexFile)
+        # dlg = SelectDialog()
+        # dlg.exec_()
 
-        #Bibtex parser will be here
         with open(bibtexFile) as bibtex:
+
             try:
                 bibtex_database = bibtexparser.load(bibtex)
             except Exception:
@@ -72,28 +74,22 @@ class MainWindow(QMainWindow):
             year = bibtex_database.entries[0]["year"]
             title = bibtex_database.entries[0]["title"]
             type1 = bibtex_database.entries[0]["ENTRYTYPE"]
-            print(author, year, title, type1)
-            data = {[author, year, type1, title]}
+            self.loadData(author, year, title, type1)
 
-            self.tableWidget.setRowCount(0)
-            for row_number, row_data in enumerate(data):
-                self.tableWidget.insertRow(row_number)
-                for column_number, data in enumerate(row_data):
-                    self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
-    # def setData(self):
-    #
-    #     for m, item in enumerate(self.data[ ]):
-    #         newitem = QTableWidgetItem(item)
-    #         self.setItem(m, n, newitem)
-    #         # self.tableWidget.setRowCount(0)
-    #         # for row_number, row_data in type1(4, ):
-    #         #     self.tableWidget.insertRow(row_number)
-    #         #     for column_number, data in enumerate(author, year, type1, title):
-    #         #         self.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
-    #
-    #         print(author, year, title, type1)
+            print(author)
+            print(year)
+            print(title)
+            print(type1)
+
+class CreateDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(CreateDialog, self).__init__(*args, **kwargs)
+        # Code of Necati Ozkent will be here
+
+
 
 app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
+if (QDialog.Accepted == True):
+    window = MainWindow()
+    window.show()
 sys.exit(app.exec_())
