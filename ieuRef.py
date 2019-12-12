abc1d46648a7cb6fbcd190bf24f30df902f1730e
sercankavdir <35980788+sercankavdir@users.noreply.github.com>
2019-12-12 11:09:58 +0300

    GUI toolbar added, dummy data included for search and task parts divided

--- ieuRef.py
+++ ieuRef.py
@@ -12,7 +12,7 @@ class MainWindow(QMainWindow):
 
         help_menu = self.menuBar().addMenu("&About")
         self.setWindowTitle("IeuRef")
-        self.setMinimumSize(900, 700)
+        self.setMinimumSize(1000, 800)
 
         self.tableWidget = QTableWidget()
         self.setCentralWidget(self.tableWidget)
@@ -37,20 +37,71 @@ class MainWindow(QMainWindow):
         selectBibtex_action.triggered.connect(self.selectBibtex)
         file_menu.addAction(selectBibtex_action)
 
-        createbibtex_action = QAction(QIcon("icon/create.png"),"Create Bibtex", self)
-        createbibtex_action.triggered.connect(self.createBibtex)
-        file_menu.addAction(createbibtex_action)
+        createBibtex_action = QAction(QIcon("icon/create.png"), "Create Bibtex", self)
+        createBibtex_action.triggered.connect(self.createBibtex)
+        file_menu.addAction(createBibtex_action)
 
-    def loadData(self, a, b, c, d):
-        self.tableWidget.setItem(1, 0, QTableWidgetItem(a))
-        self.tableWidget.setItem(1, 1, QTableWidgetItem(b))
-        self.tableWidget.setItem(1, 2, QTableWidgetItem(c))
-        self.tableWidget.setItem(1, 3, QTableWidgetItem(d))
+        searchBibtex_action = QAction(QIcon("icon/search.png"), "Search Student", self)
+        searchBibtex_action.triggered.connect(self.search)
+        file_menu.addAction(searchBibtex_action)
+
+        btn_selectBibtex_action = QAction(QIcon("icon/addBibtex.png"), "Select BibTeX file", self)
+        btn_selectBibtex_action.triggered.connect(self.selectBibtex)
+        btn_selectBibtex_action.setStatusTip('Select BibTeX File')
+        toolbar.addAction(btn_selectBibtex_action)
+
+        btn_createBibtex_action = QAction(QIcon("icon/create.png"), "Create Bibtex", self)
+        btn_createBibtex_action.triggered.connect(self.createBibtex)
+        btn_createBibtex_action.setStatusTip('Create BibTeX Entry')
+        toolbar.addAction(btn_createBibtex_action)
+
+        btn_searchBibtex_action = QAction(QIcon("icon/search.png"), "Search Student", self)
+        btn_searchBibtex_action.triggered.connect(self.search)
+        btn_searchBibtex_action.setStatusTip('Search')
+        toolbar.addAction(btn_searchBibtex_action)
+
+    def loaddata(self):
+        # Dummy data for searching
+        self.tableWidget.setRowCount(5)
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("1"))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem("Cai, Hongyun and Zheng, Vincent W. and Zhu, Fanwei and Chang, Kevin Chen-Chuan and Huang, Zi"))
+        self.tableWidget.setItem(0, 2, QTableWidgetItem("2017"))
+        self.tableWidget.setItem(0, 3, QTableWidgetItem("article"))
+        self.tableWidget.setItem(0, 4, QTableWidgetItem("From Community Detection to Community Profiling"))
+
+        self.tableWidget.setItem(1, 0, QTableWidgetItem("2"))
+        self.tableWidget.setItem(1, 1, QTableWidgetItem("Jantz, Michael R. and Robinson, Forrest J. and Kulkarni, Prasad A."))
+        self.tableWidget.setItem(1, 2, QTableWidgetItem("2016"))
+        self.tableWidget.setItem(1, 3, QTableWidgetItem("article"))
+        self.tableWidget.setItem(1, 4, QTableWidgetItem("Impact of Intrinsic Profiling Limitations on Effectiveness of Adaptive Optimizations"))
+
+        self.tableWidget.setItem(2, 0, QTableWidgetItem("3"))
+        self.tableWidget.setItem(2, 1, QTableWidgetItem("Sharma, Sanket S. and De Choudhury, Munmun"))
+        self.tableWidget.setItem(2, 2, QTableWidgetItem("2015"))
+        self.tableWidget.setItem(2, 3, QTableWidgetItem("inproceedings"))
+        self.tableWidget.setItem(2, 4, QTableWidgetItem("Measuring and Characterizing Nutritional Information of Food and Ingestion Content in Instagram"))
+
+        self.tableWidget.setItem(3, 0, QTableWidgetItem("4"))
+        self.tableWidget.setItem(3, 1, QTableWidgetItem("Zhan, Ming and Tu, Ruibo and Yu, Qin"))
+        self.tableWidget.setItem(3, 2, QTableWidgetItem("2018"))
+        self.tableWidget.setItem(3, 3, QTableWidgetItem("inproceedings"))
+        self.tableWidget.setItem(3, 4, QTableWidgetItem("Understanding Readers: Conducting Sentiment Analysis of Instagram Captions"))
+
+        self.tableWidget.setItem(4, 0, QTableWidgetItem("5"))
+        self.tableWidget.setItem(4, 1, QTableWidgetItem("Schlauch, Wolfgang E. and Zweig, Katharina A. and Theory, Graph and Analysis, Network"))
+        self.tableWidget.setItem(4, 2, QTableWidgetItem("2015"))
+        self.tableWidget.setItem(4, 3, QTableWidgetItem("inproceedings"))
+        self.tableWidget.setItem(4, 4, QTableWidgetItem("Influence of the Null-Model on Motif Detection"))
+
 
 
     def createBibtex(self):
         dlg = CreateDialog()
         dlg.exec()
+
+    def search(self):
+        dlg = SearchDialog()
+        dlg.exec_()
 
     def selectBibtex(self):
         options = QFileDialog.Options()
@@ -84,7 +135,16 @@ class MainWindow(QMainWindow):
 class CreateDialog(QDialog):
     def __init__(self, *args, **kwargs):
         super(CreateDialog, self).__init__(*args, **kwargs)
-        # Code of Necati Ozkent will be here
+        #Necati Ozkent Task
+        #Creating entry code will be here
+        #Different types will have different fields
+
+
+class SearchDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(SearchDialog, self).__init__(*args, **kwargs)
+        #Pelinsu Arslan Task
+        #Search code will be here
 
 
 
@@ -92,4 +152,5 @@ app = QApplication(sys.argv)
 if (QDialog.Accepted == True):
     window = MainWindow()
     window.show()
+    window.loaddata()
 sys.exit(app.exec_())
